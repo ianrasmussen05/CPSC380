7b6e41e0d53add481eab07cdd8b9c2f365aff184
ianrasmussen05 <irasmussen@chapman.edu>
2021-10-26 09:38:58 -0700

    First commit with checking the correct arguments inputed.

---
+++ Assignment4/README.md
@@ -0,0 +1,21 @@
+# Assignment 4 - Thread Synchronization
+
+(describe program here)
+
+* Ian Rasmussen
+* 2317200
+* irasmussen@chapman.edu
+* CPSC 380-01
+
+## Source Files
+* prodCon.c
+
+## Known Errors
+* 
+
+## References
+* 
+
+# Execute Program
+* gcc -D_REENTRANT prodCon.c –o prodCon -lpthread
+* ./prodCon (keyfile) (number of items) (number of times)
---
+++ Assignment4/prodCon.c
@@ -0,0 +1,89 @@
+/*
+ * Ian Rasmussen
+ * 2317200
+ * irasmussen@chapman.edu
+ * CPSC 380-01
+ * Assignment 4 - Thread Synchronization
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 256
+#define MEM_SIZE 262144
+
+struct Buffer
+{
+    long exec;
+    char buff[BUFFER_SIZE];
+};
+
+pthread_t tid[2];
+pthread_mutex_t mutex1;
+
+void *producer(void *arg)
+{
+
+
+    return NULL;
+}
+
+void *consumer(void *arg)
+{
+
+
+    return NULL;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int runLoop = 1;
+
+    if (argc < 4)
+    {
+        printf("3 arguments as input is required.\n");
+        printf("Needed: <Filename> <numItems> <numTimes>\n");
+        return -1;
+    }
+
+    char *fileName = argv[1];
+    int numItems, numTimes; 
+
+    if (atoi(argv[2]) > 0) // Checking if the second argument is an integer > 0
+    {
+        numItems = atoi(argv[2]);
+    }
+    else 
+    {
+        printf("The second argument is not an integer value or inputted a 0.\n");
+        return -1;
+    }
+
+    if (atoi(argv[3]) > 0) // Checking if third argument is an integer > 0
+    {
+        numTimes = atoi(argv[3]);
+    }
+    else
+    {
+        printf("The third argument is not an integer value or inputted a 0.\n");
+        return -1;
+    }
+
+    printf("%s\n", fileName);
+    printf("%d\n", numItems);
+    printf("%d\n", numTimes);
+
+    while (runLoop)
+    {
+
+        return 1;
+    }
+
+    return 0;
+}
