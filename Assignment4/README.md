7785c0fff8ab71ef9f9ab41b69b0e8f1e65b8a7a
ianrasmussen05 <irasmussen@chapman.edu>
2021-10-26 21:55:36 -0700

    Updated all the files within the assignment. Did not fully complete assignment. Look more into README.md.

--- Assignment4/README.md
+++ Assignment4/README.md
@@ -1,6 +1,6 @@
 # Assignment 4 - Thread Synchronization
 
-(describe program here)
+This C program creates two threads, where the first thread is the consumer and consumes the data written to a shared memory buffer. The second thread is the producer that produces the data for the shared memory buffer. Meaning that the consumer reads from the shared memory and the producer writes to the memory. It uses a mutex semaphore and counting semaphore to prevent race conditions.
 
 * Ian Rasmussen
 * 2317200
@@ -8,18 +8,21 @@
 * CPSC 380-01
 
 ## Source Files
-* prodCon.c
+* prodcon.c
 * MakeFile
 
 ## Known Errors
-* 
+* The error checking in this program works for the inputs.
+* However, the program will not complete its calculations and seems to run forever.
+* I am unsure to why it is not working as I thought I had all the checksum calculations correct.
 
 ## References
-* 
+* Got some of the code from the shm zip file from class.
+* To find out how to allocate memory in C: https://www.tutorialspoint.com/c_standard_library/c_function_malloc.htm 
 
 ## Execute Program
 * make
-* ./prodCon (keyfile) (number of items) (number of times)
+* ./prodcon (keyfile) (number of items) (number of times)
 
 ## Clean Directory
 * make clean
---
+++ Assignment4/prodcon.c
@@ -0,0 +1,201 @@
+/*
+ * Ian Rasmussen
+ * 2317200
+ * irasmussen@chapman.edu
+ * CPSC 380-01
+ * Assignment 4 - Thread Synchronization
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <semaphore.h>
+
+#define BUFFER_SIZE 32
+#define RAND_DATA 256
+
+int memSize, numItems, numTimes;
+unsigned short cksum1, cksum2;
+char *fileName = "nothing.txt";
+char *memory;
+pthread_t tid[2];
+sem_t sem[2]; // 2 semaphores for 2 threads
+pthread_mutex_t m_mutex;
+
+void *producer(void *arg)
+{
+    // Number of times to write from the shared buffer
+    for (int i = 0; i < numTimes; ++i)
+    {
+        // Got how to use sem_wait and sem_post here: https://stackoverflow.com/questions/15182328/semaphore-implementation
+        sem_wait(&sem[0]); // Uses the first semaphore for writing
+
+        // Number of buffers within memory
+        for (int j = 0; j < numItems; ++j)
+        {
+            // How to use mutex locks: https://www.man7.org/linux/man-pages/man3/pthread_mutex_lock.3p.html
+            pthread_mutex_lock(&m_mutex);
+
+            cksum1 = 0;
+            // This is where we write the data
+            for (int k = j * BUFFER_SIZE; k < ((j + 1) * BUFFER_SIZE) - 2; ++k) // Go to specific spot in memory
+            {
+                memory[k] = rand() % RAND_DATA;
+                cksum1 += memory[k]; // This calculates the first checksum
+            }
+
+            // Got the next lines from the shmwrite.c file used in class
+            cksum1 = checksum(&memory[0], BUFFER_SIZE - 2); 
+            memcpy(&memory[BUFFER_SIZE-2], &cksum1, 2);
+
+            // Unlocks the mutex to go to the next semaphore
+            pthread_mutex_unlock(&m_mutex);
+            sem_post(&sem[1]); // Switch to second semaphore
+        }
+    }
+
+    return NULL;
+}
+
+void *consumer(void *arg)
+{
+    // Number of times to write from the shared buffer
+    for (int i = 0; i < numTimes; ++i)
+    {
+        // Got how to use sem_wait and sem_post here: https://stackoverflow.com/questions/15182328/semaphore-implementation
+        sem_wait(&sem[1]); // Have this go for the second semaphore, reads data
+
+        // Number of buffers within memory
+        for (int j = 0; j < numItems; ++j)
+        {
+            pthread_mutex_lock(&m_mutex); // Locks the thread in the data space
+
+            cksum2 = 0;
+            for (int k = j * BUFFER_SIZE; k < ((j + 1) * BUFFER_SIZE) - 2; ++k) // Go to specific spot in memory
+            {
+                cksum2 += memory[k]; // This calculates the next checksum
+            }
+
+            // Got these next few lines from the shm.zip folder in the shmread.c file
+            if (cksum1 != cksum2)
+            {
+                printf("Checksum mismatch: expected %02x, received %02x \n", cksum2, cksum1);
+                exit(0);
+            }
+
+            pthread_mutex_unlock(&m_mutex);
+            sem_post(&sem[0]); // Switch back to first semaphore
+        }
+    }
+
+    printf("The checksums match!\n");
+
+    return NULL;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int threadCheck;
+
+    if (argc < 4)
+    {
+        printf("3 arguments as input is required.\n");
+        printf("Needed: ./prodCon <Filename> <numItems> <numTimes>\n");
+        return -1;
+    }
+
+    fileName = argv[1];
+
+    // Checking if the second argument is an integer > 0
+    if (atoi(argv[2]) > 0) 
+    {
+        memSize = atoi(argv[2]);
+
+        // Checking to see if the value inputted is divisible by 32 and no more than 1024 buffers
+        if (memSize % 32 == 0 && memSize <= 32768) 
+        {
+            numItems = memSize / 32; // The number of buffers that is inputted from memory
+
+            // Used this link for allocating memory: https://www.tutorialspoint.com/c_standard_library/c_function_malloc.htm
+            memory = malloc(memSize); // The actual size of memory that was inputted
+        }
+        else 
+        {
+            printf("The second argument must be divisible by 32.\n");
+            return -1;
+        }
+    }
+    else 
+    {
+        printf("The second argument is not an integer value or inputted a 0.\n");
+        return -1;
+    }
+
+    // Checking if third argument is an integer > 0
+    if (atoi(argv[3]) > 0) 
+    {
+        numTimes = atoi(argv[3]);
+    }
+    else
+    {
+        printf("The third argument is not an integer value or inputted a 0.\n");
+        return -1;
+    }
+
+    // This initializes both semaphores
+    for (int i = 0; i <= 1; ++i)
+    {
+        if (sem_init(&sem[i], 0, 0) < 0)
+        {
+            printf("Error initializing semaphore %d: %d", i, errno);
+            return -1;
+        }
+    }
+
+    /* Create the mutex semaphore to protect the critical section */
+    if (pthread_mutex_init(&m_mutex, NULL) < 0) 
+    {
+        printf("Error creating mutex\n");
+        return -1;
+    }
+
+    // Creating and checking the threads 
+    threadCheck = pthread_create(&tid[0], NULL, &producer, NULL);
+    if (threadCheck < 0) 
+    {
+        printf("Can't create thread 1 :[%s]\n", strerror(threadCheck));
+        return -1;
+    }
+
+    threadCheck = pthread_create(&tid[1], NULL, &consumer, NULL);
+    if (threadCheck < 0) 
+    {
+        printf("Can't create thread 2 :[%s]\n", strerror(threadCheck));
+        return -1;
+    }
+
+    // Join the threads
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+
+    // Destroy semaphores
+    sem_destroy(&sem[0]);
+    sem_destroy(&sem[1]);
+
+    // Reference: https://www.tutorialspoint.com/c_standard_library/c_function_malloc.htm
+    free(memory); // Frees up the allocated memory
+
+    // Destroy mutex pthread
+    pthread_mutex_destroy(&m_mutex);
+
+
+    return 0;
+}
