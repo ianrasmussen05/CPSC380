8a800769124114abe7ff986c95de55fc908fe056
ianrasmussen05 <irasmussen@chapman.edu>
2021-11-08 13:15:16 -0800

    Added Backing store file and updated the vmmgr.cpp ile.

--- Assignment5/vmmgr.cpp
+++ Assignment5/vmmgr.cpp
@@ -1,21 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 #include <iostream>
 #include <string>
 
+#define PAGE_SIZE 256
+#define FRAMES 256
+#define FRAME_SIZE 256
+#define TABLE_SIZE 16
+
 int main (int argc, char* argv[])
 {
-    std::string userInput = "";
-
-    std::cout << "Enter an input: ";
-    std::cin >> userInput;
-
-    while (userInput.empty())
+    // Checks to see if there are the correct number of inputs
+    if (argc < 2)
     {
-        std::cout << "Must enter a value." << std::endl;
-        std::cin >> userInput;
-        std::cout << std::endl;
+        std::cout << "Enter a file to read from in the command line." << std::endl;
+        return -1;
     }
 
-    std::cout << "You entered: " + userInput << std::endl;
+    std::string fileName = argv[1];
+
+    FILE *file;
+    file = fopen(fileName, "r"); // Open file and read from it.
+
+    if (file != NULL) // There are contents within the file
+    {
+        // Now we must read each line
+        char *line = NULL;
+        int readResult;
+
+        readResult = fread(line, sizeof(char), PAGE_SIZE, file);
+
+
+
+        free(line); // Free the memory of this variable
+    }
+    else 
+    {
+        std::cout << "The file inputted is empty. Enter the correct file." << std::endl;
+        return -1;
+    }
+
+
+    fclose(file);
 
     return 0;
 }
